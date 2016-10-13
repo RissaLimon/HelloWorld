e7846d5cd4222eee5e9f29a74dcbaec61124c8fe
rlimon5579 <rlimon5579@001LAB153010829.neisd.ad>
2016-10-13 15:49:10 -0500

    Initial Commit

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,74 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// lel
+		System.out.println("My name is Rissa Limon ");
+		System.out.println("My Birthday is July 29th, 2002");
+		System.out.print("The remainder of 2016 and 29 is ");
+		System.out.println(2016/29);
+		
+		System.out.println("l");
+		System.out.println("le");
+		System.out.println("leo");
+		System.out.println("leon");
+		System.out.println("leona");
+		System.out.println("leonar");
+		System.out.println("leonard");
+		System.out.println("leonardo");
+		System.out.println("leonardo d");
+		System.out.println("leonardo da");
+		System.out.println("leonardo da v");
+		System.out.println("leonardo da vi");
+		System.out.println("leonardo da vin");
+		System.out.println("leonardo da vinc");
+		System.out.println("leonardo da vinci");
+		System.out.println(" leonardo da vinci");
+		System.out.println("  leonardo da vinci");
+		System.out.println("   leonardo da vinci");
+		System.out.println("    leonardo da vinci");
+		System.out.println("     leonardo da vinci");
+		System.out.println("      leonardo da vinci");
+		System.out.println("       leonardo da vinci");
+		System.out.println("        leonardo da vinci");
+		System.out.println("         leonardo da vinci");
+		System.out.println("          leonardo da vinci");
+		System.out.println("           leonardo da vinci");
+		System.out.println("          leonardo da vinci");
+		System.out.println("         leonardo da vinci");
+		System.out.println("        leonardo da vinci");
+		System.out.println("       leonardo da vinci");
+		System.out.println("      leonardo da vinci");
+		System.out.println("     leonardo da vinci");
+		System.out.println("    leonardo da vinci");
+		System.out.println("   leonardo da vinci");
+		System.out.println("  leonardo da vinci");
+		System.out.println(" leonardo da vinci");
+		System.out.println("leonardo da vinci");
+		System.out.println("leonardo da vinc");
+		System.out.println("leonardo da vin");
+		System.out.println("leonardo da vi");
+		System.out.println("leonardo da v");
+		System.out.println("leonardo da ");
+		System.out.println("leonardo d");
+		System.out.println("leonardo ");
+		System.out.println("leonard");
+		System.out.println("leonar");
+		System.out.println("leona");	
+		System.out.println("leon");
+		System.out.println("leo");
+		System.out.println("le");
+		System.out.println("l");
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+
